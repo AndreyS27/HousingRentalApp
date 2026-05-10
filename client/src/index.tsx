59dc4b6b6023a0a9c89416cb236dca7382af8134
Andrey S <salihov.andrey1@yandex.ru>
2026-05-10 10:56:20 +0300

    feat: add redux to app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+import { store } from './store';
 import App from './App';
+import './index.css';
+
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/notifications/styles.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <MantineProvider>
+          <Notifications />
+          <App />
+        </MantineProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
