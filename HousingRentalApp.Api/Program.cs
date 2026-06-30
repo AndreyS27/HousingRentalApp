e2ba77e006ca3b4c4a645d431944e0bbd420ce01
Andrey <salihov.andrey1@yandex.ru>
2026-06-30 09:37:51 +0300

    test 8

--- HousingRentalApp.Api/Program.cs
+++ HousingRentalApp.Api/Program.cs
@@ -13,177 +13,177 @@ namespace HousingRentalApp.Api
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                var builder = WebApplication.CreateBuilder(args);
-
-                //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-                //    options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") ?? throw new Exception("Connection string is not defined")));
-
-                builder.WebHost.UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000");
-
-                //builder.Configuration.AddEnvironmentVariables();
-
-                builder.Services.AddControllers();
-                builder.Services.AddEndpointsApiExplorer();
-                builder.Services.AddSwaggerGen();
-
-                var app = builder.Build();
-
-                var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
-                lifetime.ApplicationStopping.Register(() =>
-                {
-                    Console.WriteLine("=== ApplicationStopping triggered ===");
-                });
-                lifetime.ApplicationStopped.Register(() =>
-                {
-                    Console.WriteLine("=== ApplicationStopped triggered ===");
-                });
-
-
-                app.UseStaticFiles();
-
-                app.UseSwagger();
-                app.UseSwaggerUI();
-
-                app.MapControllers();
-
-                Console.WriteLine("=== Starting minimal app v6 ===");
-                app.Run();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("=== FATAL ERROR in app.Run ===");
-                Console.WriteLine(ex.ToString());
-                throw;
-            }
-
             //try
             //{
             //    var builder = WebApplication.CreateBuilder(args);
 
-            //    builder.Configuration.AddEnvironmentVariables();
+            //    //builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            //    //    options.UseNpgsql(Environment.GetEnvironmentVariable("ConnectionStrings__DefaultConnection") ?? throw new Exception("Connection string is not defined")));
 
-            //    // временные логи:
-            //    Console.WriteLine("=== APPLICATION STARTING ===");
-            //    Console.WriteLine($"Connection string: {builder.Configuration.GetConnectionString("DefaultConnection")}");
+            //    builder.WebHost.UseUrls(Environment.GetEnvironmentVariable("ASPNETCORE_URLS") ?? "http://localhost:5000");
 
-            //    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+            //    //builder.Configuration.AddEnvironmentVariables();
 
-            //    builder.Services.AddScoped<IUserRepository, UserRepository>();
-            //    builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
-            //    builder.Services.AddScoped<IAuthService, AuthService>();
-            //    builder.Services.AddScoped<IPropertyService, PropertyService>();
-            //    builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
-            //    builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-            //    builder.Services.AddScoped<IBookingService, BookingService>();
-            //    builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
-            //    builder.Services.AddScoped<IReviewService, ReviewService>();
-            //    builder.Services.AddScoped<IPaymentService, PaymentService>();
-            //    builder.Services.AddScoped<IPropertyViewRepository, PropertyViewRepository>();
-
-            //    // Настройка JWT аутентификации
-            //    var jwtSecret = builder.Configuration["JwtSettings:Secret"];
-            //    var key = Encoding.ASCII.GetBytes(jwtSecret);
-
-            //    builder.Services.AddAuthentication(options =>
-            //    {
-            //        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            //        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            //    })
-            //    .AddJwtBearer(options =>
-            //    {
-            //        options.RequireHttpsMetadata = false; // поменять в продакшене
-            //        options.SaveToken = true;
-
-            //        options.TokenValidationParameters = new TokenValidationParameters
-            //        {
-            //            ValidateIssuerSigningKey = true,
-            //            IssuerSigningKey = new SymmetricSecurityKey(key),
-            //            ValidateIssuer = true,
-            //            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-
-            //            ValidateAudience = true,
-            //            ValidAudience = builder.Configuration["JwtSettings:Audience"],
-
-            //            ValidateLifetime = true,
-            //            ClockSkew = TimeSpan.Zero
-            //        };
-            //    });
-
-            //    builder.Services.AddAuthorization();
             //    builder.Services.AddControllers();
             //    builder.Services.AddEndpointsApiExplorer();
-
-            //    builder.Services.AddSwaggerGen(c =>
-            //    {
-            //        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            //        {
-            //            Name = "Authorization",
-            //            Type = SecuritySchemeType.ApiKey,
-            //            Scheme = "Bearer",
-            //            BearerFormat = "JWT",
-            //            In = ParameterLocation.Header,
-            //            Description = "Enter 'Bearer [token]'"
-            //        });
-            //        c.AddSecurityRequirement(new OpenApiSecurityRequirement
-            //    {
-            //        {
-            //            new OpenApiSecurityScheme
-            //            {
-            //                Reference = new OpenApiReference
-            //                {
-            //                    Type = ReferenceType.SecurityScheme,
-            //                    Id = "Bearer"
-            //                }
-            //            },
-            //            new string[] {}
-            //        }
-            //    });
-            //    });
-
-            //    // CORS
-            //    builder.Services.AddCors(options =>
-            //    {
-            //        options.AddPolicy("AllowReactApp", policy =>
-            //        {
-            //            policy.WithOrigins(
-            //                "http://localhost:3000",
-            //                "https://housing-rental-app.vercel.app",
-            //                "https://housing-rental-aszuijnk3-andreys27s-projects.vercel.app/")
-            //                .AllowAnyHeader()
-            //                .AllowAnyMethod()
-            //                .AllowCredentials();
-            //        });
-            //    });
+            //    builder.Services.AddSwaggerGen();
 
             //    var app = builder.Build();
 
-            //    if (app.Environment.IsDevelopment())
+            //    var lifetime = app.Services.GetRequiredService<IHostApplicationLifetime>();
+            //    lifetime.ApplicationStopping.Register(() =>
             //    {
-            //        app.UseSwagger();
-            //        app.UseSwaggerUI();
-            //    }
+            //        Console.WriteLine("=== ApplicationStopping triggered ===");
+            //    });
+            //    lifetime.ApplicationStopped.Register(() =>
+            //    {
+            //        Console.WriteLine("=== ApplicationStopped triggered ===");
+            //    });
 
-            //    app.UseCors("AllowReactApp");
-            //    //app.UseHttpsRedirection();
 
-            //    app.UseAuthentication();
-            //    app.UseAuthorization();
-
-            //    app.MapControllers();
             //    app.UseStaticFiles();
 
-            //    Console.WriteLine("=== Application configured, starting run ===");
+            //    app.UseSwagger();
+            //    app.UseSwaggerUI();
+
+            //    app.MapControllers();
+
+            //    Console.WriteLine("=== Starting minimal app v6 ===");
             //    app.Run();
             //}
             //catch (Exception ex)
             //{
-            //    Console.WriteLine($"FATAL ERROR: {ex.Message}");
-            //    Console.WriteLine(ex.StackTrace);
+            //    Console.WriteLine("=== FATAL ERROR in app.Run ===");
+            //    Console.WriteLine(ex.ToString());
             //    throw;
             //}
+
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Configuration.AddEnvironmentVariables();
+
+                // временные логи:
+                Console.WriteLine("=== APPLICATION STARTING ===");
+                Console.WriteLine($"Connection string: {builder.Configuration.GetConnectionString("DefaultConnection")}");
+
+                builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+                builder.Services.AddScoped<IUserRepository, UserRepository>();
+                builder.Services.AddScoped<IPropertyRepository, PropertyRepository>();
+                builder.Services.AddScoped<IAuthService, AuthService>();
+                builder.Services.AddScoped<IPropertyService, PropertyService>();
+                builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+                builder.Services.AddScoped<IBookingRepository, BookingRepository>();
+                builder.Services.AddScoped<IBookingService, BookingService>();
+                builder.Services.AddScoped<IReviewRepository, ReviewRepository>();
+                builder.Services.AddScoped<IReviewService, ReviewService>();
+                builder.Services.AddScoped<IPaymentService, PaymentService>();
+                builder.Services.AddScoped<IPropertyViewRepository, PropertyViewRepository>();
+
+                // Настройка JWT аутентификации
+                var jwtSecret = builder.Configuration["JwtSettings:Secret"];
+                var key = Encoding.ASCII.GetBytes(jwtSecret);
+
+                builder.Services.AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.RequireHttpsMetadata = false; // поменять в продакшене
+                    options.SaveToken = true;
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = true,
+                        ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+
+                        ValidateAudience = true,
+                        ValidAudience = builder.Configuration["JwtSettings:Audience"],
+
+                        ValidateLifetime = true,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+                builder.Services.AddAuthorization();
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+
+                builder.Services.AddSwaggerGen(c =>
+                {
+                    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "Enter 'Bearer [token]'"
+                    });
+                    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+                });
+
+                // CORS
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy("AllowReactApp", policy =>
+                    {
+                        policy.WithOrigins(
+                            "http://localhost:3000",
+                            "https://housing-rental-app.vercel.app",
+                            "https://housing-rental-aszuijnk3-andreys27s-projects.vercel.app/")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .AllowCredentials();
+                    });
+                });
+
+                var app = builder.Build();
+
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseCors("AllowReactApp");
+                //app.UseHttpsRedirection();
+
+                app.UseAuthentication();
+                app.UseAuthorization();
+
+                app.MapControllers();
+                app.UseStaticFiles();
+
+                Console.WriteLine("=== Application configured, starting run v7 ===");
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"FATAL ERROR: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                throw;
+            }
         }
     }
 }
